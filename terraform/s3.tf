3c549770483123d5400151d74df5da92a6158fd6
James Geddes <j@jamesgeddes.pro>
2023-11-06 09:11:55 +0000

    set up state bucket

---
+++ terraform/s3.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = var.state_bucket
+  tags = {
+    Name        = var.state_bucket
+    Environment = "Estate"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_versioning" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_sse" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
