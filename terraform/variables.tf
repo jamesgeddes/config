b677021258c9af253258dd956d5aefaa5fc3d4ed
James Geddes <j@jamesgeddes.pro>
2023-11-06 09:12:05 +0000

    set up vars

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "state_bucket" {
+  description = "The name of the Terraform state bucket"
+  type = string
+}
+
+variable "owner" {
+  description = "The user or org who owns this repo"
+  type = string
+}
